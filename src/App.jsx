634be10652e77fd7caa7e5b958ef54b323ed1cf9
Pranav <bizzareforyou@gmail.com>
2026-04-16 16:20:19 +0530

    added joke component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import SubmissionForm from "./components/01_submission_form/SubmissionForm";
+import Joke from "./components/02_joke_generator/Joke";
 
 function App() {
-  return <SubmissionForm />;
+  return (
+    <>
+      {/* <SubmissionForm /> */}
+      <Joke />
+    </>
+  );
 }
 
 export default App;
