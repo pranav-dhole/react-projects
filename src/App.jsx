baff3d23d12a77d807c4bcc6c04f2b54312c8940
Pranav <bizzareforyou@gmail.com>
2026-04-18 16:48:29 +0530

    added PasswordStrength component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SubmissionForm from "./components/01_submission_form/SubmissionForm";
 import Joke from "./components/02_joke_generator/Joke";
+import PasswordStrength from "./components/03_password_strength/passwordStrength";
 
 function App() {
   return (
     <>
       {/* <SubmissionForm /> */}
-      <Joke />
+      {/* <Joke /> */}
+      <PasswordStrength />
     </>
   );
 }
