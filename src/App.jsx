86db74b6c138d244fb1a16e67fc54921caf46dde
Pranav <bizzareforyou@gmail.com>
2026-04-19 17:08:13 +0530

    added DiceRoll component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import SubmissionForm from "./components/01_submission_form/SubmissionForm";
 import Joke from "./components/02_joke_generator/Joke";
 import PasswordStrength from "./components/03_password_strength/passwordStrength";
+import DiceRoll from "./components/04_dice_roll/DiceRoll";
 
 function App() {
   return (
     <>
       {/* <SubmissionForm /> */}
       {/* <Joke /> */}
-      <PasswordStrength />
+      {/* <PasswordStrength /> */}
+      <DiceRoll />
     </>
   );
 }
