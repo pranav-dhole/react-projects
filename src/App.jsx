0354ea7d4d4393a1e38b9d71d2907249ad2dba22
Pranav <bizzareforyou@gmail.com>
2026-04-20 18:50:09 +0530

    added RockPaperScissors component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import SubmissionForm from "./components/01_submission_form/SubmissionForm";
 import Joke from "./components/02_joke_generator/Joke";
 import PasswordStrength from "./components/03_password_strength/passwordStrength";
 import DiceRoll from "./components/04_dice_roll/DiceRoll";
+import RockPaperScissors from "./components/05_rock_paper_scissors/RockPaperScissors";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <SubmissionForm /> */}
       {/* <Joke /> */}
       {/* <PasswordStrength /> */}
-      <DiceRoll />
+      {/* <DiceRoll /> */}
+      <RockPaperScissors />
     </>
   );
 }
