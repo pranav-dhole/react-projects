fc318f4057cd849edba7e17b68d6b289e9b113f8
Pranav <bizzareforyou@gmail.com>
2026-04-27 21:17:20 +0530

    completed FoodRecipe component

--- src/components/11_food_recipe/FoodRecipe.jsx
+++ src/components/11_food_recipe/FoodRecipe.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./foodRecipe.css";
 
 const FoodRecipe = () => {
@@ -13,8 +13,12 @@ const FoodRecipe = () => {
           `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`,
         );
         const data = await res.json();
-        console.log(data?.meals);
         setMeals(data?.meals);
+        if (!data?.meals) {
+          setErr("No Recipe Found For Your Dish :{");
+        } else {
+          setErr("");
+        }
       } catch (err) {
         console.log(err.message);
       }
@@ -34,14 +38,46 @@ const FoodRecipe = () => {
         <button onClick={fetchMeals}>Search</button>
       </div>
 
+      <h3 className="err-msg">{err}</h3>
       {meals && (
-        <div>
-          {meals.map((meal) => (
-            <div>
-              <p>{meal.strArea}</p>
-              <p>{meal.strInstructions}</p>
-            </div>
-          ))}
+        <div className="meals-grid">
+          {meals.map((meal) => {
+            const ingredients = [];
+            for (let i = 1; i <= 20; i++) {
+              const ingredient = meal[`strIngredient${i}`];
+              const measure = meal[`strMeasure${i}`];
+
+              if (ingredient && ingredient.trim() !== "") {
+                ingredients.push({
+                  name: ingredient,
+                  measure: measure,
+                });
+              }
+            }
+
+            return (
+              <div key={meal.idMeal}>
+                <div className="card-container">
+                  <img src={meal?.strMealThumb} className="meal-img" />
+                  <div className="card-header">
+                    <h3>{meal.strMeal}</h3>
+                    <span className="region-tag">{meal.strArea}</span>
+                  </div>
+
+                  <div className="card-body">
+                    <strong>Ingredients:</strong>
+                    <ul>
+                      {ingredients.map((item, index) => (
+                        <li key={index}>
+                          {item.measure} {item.name}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
