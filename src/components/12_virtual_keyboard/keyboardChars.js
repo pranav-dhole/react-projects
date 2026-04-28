3fa83fd6f495e67390c2aa6437c5a16f3cb9c4ec
Pranav <bizzareforyou@gmail.com>
2026-04-28 19:03:17 +0530

    added all key names for separate rows

---
+++ src/components/12_virtual_keyboard/keyboardChars.js
@@ -0,0 +1,66 @@
+export const firstRow = [
+  "~.`",
+  "!.1",
+  "@.2",
+  "#.3",
+  "$.4",
+  "%.5",
+  "^.6",
+  "&.7",
+  "*.8",
+  "(.9",
+  ").0",
+  "_.-",
+  "+.=",
+  "Backspace",
+];
+
+export const secondRow = [
+  "Tab",
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "{_[",
+  "}_]",
+  "|_\\",
+];
+
+export const thirdRow = [
+  "Caps Lock",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  ":_;",
+  `"_'`,
+  "Enter",
+];
+
+export const fourthRow = [
+  "Shift",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+  "<_,",
+  ">_.",
+  "?_/",
+  "Shift",
+];
+
+export const fifthRow = ["Ctrl", "Alt", " ", "Ctrl", "Alt", "<", ">"];
